45280727fabf10fe82afa5964de68627c0656cb7
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2021-11-03 21:33:00 +0900

    class化

--- NightOfWar_wood3.py
+++ NightOfWar_wood3.py
@@ -4,32 +4,98 @@ import math
 # Jrke's special
 # -Kill your enemy soldiers or Have more bucks than your enemy at end of game
 
-my_id = int(input())  # Your unique player Id
-map_size = int(input())  # the size of map MapSize*MapSize
+def distance(my_x, my_y, opp_x, opp_y):
+    return abs(my_x - opp_x) + abs(my_y - opp_y)
 
-# game loop
-while True:
-    my_bucks = int(input())  # Your Money
-    opp_bucks = int(input())  # Opponent Money
-    for i in range(map_size):
-        for j in range(map_size):
-            # block_owner: The playerId of this box owned player
-            # x: This block's position x
-            # y: This block's position y
-            block_owner, x, y = [int(k) for k in input().split()]
-    active_soldier_count = int(input())  # Total no. of active soldier in the game
-    for i in range(active_soldier_count):
+class Board:
+	map_size = 0
+	board = []
+	def __init__(self, size):
+		Board.map_size = size
+		for _y in range(size):
+			Board.board.append([0 for _x in range(size)])
+
+	def SetOwnerID(self, block_owner, x, y):
+		Board.board[y][x] = block_owner
+
+	def GetOwnerID(self, x, y):
+		return (Board.board[y][x])
+
+	def PrintBoard(self):
+		print("board", file=sys.stderr)
+		# print(Board.board, file=sys.stderr)
+		for _y in Board.board:
+			print(_y, file=sys.stderr)
+
+class Soldier:
+    soldier_count = 0
+    soldiers = {}
+
+    def __init__(self, count):
+        Soldier.soldier_count = count
+
+    def Update(self):
+        for i in range(Soldier.soldier_count):
         # owner_id: owner of the soldier
         # x: This soldier's position x
         # y: This soldier's position y
         # soldier_id: The unique identifier of soldier
         # level: Level of the soldier ignore for first league
         # direction: The side where the soldier is facing 0 = UP, 1 = LEFT , 2 = DOWN, 3 = RIGHT
-        owner_id, x, y, soldier_id, level, direction = [int(j) for j in input().split()]
+            owner_id, x, y, soldier_id, level, direction = [int(j) for j in input().split()]
+            Soldier.soldiers[soldier_id] = [owner_id, x, y, level, direction]
+        print("Soldier dict", file=sys.stderr)
+        print(Soldier.soldiers, file=sys.stderr)
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+class NightWar:
+    my_id = int(input())
+    map_size = int(input())
+    board = Board(map_size)
+    my_bucks = 0
+    opp_bucks = 0
+    #block_owner = 0
+    active_soldier_count = 0
 
 
-    # print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE <opponent id> | SUICIDE <id>
-    print("WAIT")
+    def __init__(self):
+        print("my_id: ", NightWar.my_id, file=sys.stderr)
+        print("map size: ", NightWar.map_size, file=sys.stderr)
+
+
+    def Update(self):
+        NightWar.my_bucks = int(input())  # Your Money
+        NightWar.opp_bucks = int(input())  # Opponent Money
+        print("my_bucks: ", NightWar.my_bucks, file=sys.stderr)
+        print("opp_bucks: ", NightWar.opp_bucks, file=sys.stderr)
+
+        for i in range(NightWar.map_size):
+            for j in range(NightWar.map_size):
+                # block_owner: The playerId of this box owned player
+                # x: This block's position x
+                # y: This block's position y
+                block_owner, x, y = [int(k) for k in input().split()]
+                NightWar.board.SetOwnerID(block_owner, x, y)
+        NightWar.board.PrintBoard()
+
+        NightWar.active_soldier_count = int(input()) # Total no. of active soldier in the game
+        print("active soldier count: ", NightWar.active_soldier_count, file=sys.stderr)
+        self.soldiers = Soldier(NightWar.active_soldier_count)
+        self.soldiers.Update()
+
+
+    def Game(self):
+        self.Update()
+        self.Action()
+
+    def Action(self):
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+        # print any of actions - WAIT | MOVE <soldierId> <direction> | ATTACK <soldierID> <soldierId to attack on> | LATER > UPGRADE <id> | DEGRADE <opponent id> | SUICIDE <id>
+        print("WAIT", file=sys.stderr)
+        print("WAIT")
+
+game = NightWar()
+
+while True:
+    game.Game()
